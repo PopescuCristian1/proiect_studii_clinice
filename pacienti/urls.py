12c169b261e86117ad508f2c046f3af03ac02ae9
Cristian Popescu <popescucristian2004@gmail.com>
2025-05-13 01:59:22 +0300

    am adaugat functionalitati de cautare + editare pentru pacienti, studii si inregistrari medicale

--- pacienti/urls.py
+++ pacienti/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('inregistrari/adauga/', views.adauga_inregistrare, name='adauga_inregistrare'),
     path('inregistrari/', views.lista_inregistrari, name='lista_inregistrari'),
     path('ajax/studii/', views.studii_pentru_pacient, name='ajax_studii_pacient'),
+    path('pacienti/editeaza/<int:pacient_id>/', views.editeaza_pacient, name='editeaza_pacient'),
+    path('studii/editeaza/<int:studiu_id>/', views.editeaza_studiu, name='editeaza_studiu'),
+    path('inregistrari/editeaza/<int:inregistrare_id>/', views.editeaza_inregistrare, name='editeaza_inregistrare'),
 ]
--- pacienti/views.py
+++ pacienti/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
+from django.db import models
 
-# Create your views here.
-from django.shortcuts import render, redirect
-from .forms import PacientForm
+from .models import Pacient, StudiuClinic, InregistrareMedicala
+from .forms import PacientForm, StudiuClinicForm, InregistrareMedicalaForm
 
+# Afiseaza homepage-ul
+def home(request):
+    return render(request, 'home.html')
+
+# Adauga un pacient nou
 def adauga_pacient(request):
     if request.method == 'POST':
         form = PacientForm(request.POST)
@@ -15,20 +21,18 @@ def adauga_pacient(request):
     
     return render(request, 'pacienti/adauga_pacient.html', {'form': form})
 
-from .models import Pacient
-
+# Listeaza toti pacientii sau filtreaza dupa nume/prenume
 def lista_pacienti(request):
-    pacienti = Pacient.objects.all()
-    return render(request, 'pacienti/lista_pacienti.html', {'pacienti': pacienti})
+    query = request.GET.get('q')
+    if query:
+        pacienti = Pacient.objects.filter(
+            models.Q(nume__icontains=query) | models.Q(prenume__icontains=query)
+        )
+    else:
+        pacienti = Pacient.objects.all()
+    return render(request, 'pacienti/lista_pacienti.html', {'pacienti': pacienti, 'query': query})
 
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'home.html')
-
-from django.shortcuts import render, redirect
-from .forms import StudiuClinicForm
-
+# Adauga un nou studiu clinic
 def adauga_studiu(request):
     if request.method == 'POST':
         form = StudiuClinicForm(request.POST)
@@ -39,16 +43,12 @@ def adauga_studiu(request):
         form = StudiuClinicForm()
     return render(request, 'Studii/adauga_studiu.html', {'form': form})
 
-from .models import StudiuClinic
-
+# Listeaza toate studiile clinice
 def lista_studii(request):
     studii = StudiuClinic.objects.all()
     return render(request, 'Studii/lista_studii.html', {'studii': studii})
 
-
-from .forms import InregistrareMedicalaForm
-from .models import InregistrareMedicala
-
+# Adauga o inregistrare medicala (pacient + studiu + observatii)
 def adauga_inregistrare(request):
     if request.method == 'POST':
         form = InregistrareMedicalaForm(request.POST)
@@ -60,17 +60,79 @@ def adauga_inregistrare(request):
     
     return render(request, 'pacienti/adauga_inregistrare.html', {'form': form})
 
+# Listeaza toate inregistrarile medicale
 def lista_inregistrari(request):
     inregistrari = InregistrareMedicala.objects.select_related('pacient', 'studiu').all()
     return render(request, 'pacienti/lista_inregistrari.html', {'inregistrari': inregistrari})
 
-
-from django.http import JsonResponse
-from .models import StudiuClinic
-
+# Returneaza studiile asociate unui pacient
 def studii_pentru_pacient(request):
     pacient_id = request.GET.get('pacient_id')
     if pacient_id:
         studii = StudiuClinic.objects.filter(pacienti__id=pacient_id).values('id', 'titlu')
         return JsonResponse(list(studii), safe=False)
     return JsonResponse([], safe=False)
+
+from django.shortcuts import render, redirect, get_object_or_404
+
+def editeaza_pacient(request, pacient_id):
+    pacient = get_object_or_404(Pacient, id=pacient_id)
+    if request.method == 'POST':
+        form = PacientForm(request.POST, instance=pacient)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_pacienti')
+    else:
+        form = PacientForm(instance=pacient)
+    return render(request, 'pacienti/editeaza_pacient.html', {'form': form})
+
+
+from django.shortcuts import get_object_or_404
+from .models import StudiuClinic
+from .forms import StudiuClinicForm
+
+# Lista studiilor + cautare
+def lista_studii(request):
+    query = request.GET.get("q")
+    if query:
+        studii = StudiuClinic.objects.filter(titlu__icontains=query)
+    else:
+        studii = StudiuClinic.objects.all()
+    return render(request, 'Studii/lista_studii.html', {'studii': studii, 'query': query})
+
+# Editarea unui studiu
+def editeaza_studiu(request, studiu_id):
+    studiu = get_object_or_404(StudiuClinic, id=studiu_id)
+    if request.method == 'POST':
+        form = StudiuClinicForm(request.POST, instance=studiu)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_studii')
+    else:
+        form = StudiuClinicForm(instance=studiu)
+    return render(request, 'Studii/editeaza_studiu.html', {'form': form})
+
+
+# Lista inregistrarilor medicale + cautare
+def lista_inregistrari(request):
+    query = request.GET.get('q')
+    if query:
+        inregistrari = InregistrareMedicala.objects.select_related('pacient', 'studiu').filter(
+            models.Q(pacient__nume__icontains=query) | models.Q(pacient__prenume__icontains=query)
+        )
+    else:
+        inregistrari = InregistrareMedicala.objects.select_related('pacient', 'studiu').all()
+    
+    return render(request, 'pacienti/lista_inregistrari.html', {'inregistrari': inregistrari, 'query': query})
+
+# Editare inregistrare medicala
+def editeaza_inregistrare(request, inregistrare_id):
+    inregistrare = get_object_or_404(InregistrareMedicala, id=inregistrare_id)
+    if request.method == 'POST':
+        form = InregistrareMedicalaForm(request.POST, instance=inregistrare)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_inregistrari')
+    else:
+        form = InregistrareMedicalaForm(instance=inregistrare)
+    return render(request, 'pacienti/editeaza_inregistrare.html', {'form': form})

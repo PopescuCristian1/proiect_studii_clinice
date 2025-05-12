100d42d568abb2e07dc8f375810113fa7daf80f0
Cristian Popescu <popescucristian2004@gmail.com>
2025-05-12 22:10:57 +0300

    view pentru adaugare pacient

--- pacienti/views.py
+++ pacienti/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from .forms import PacientForm
+
+def adauga_pacient(request):
+    if request.method == 'POST':
+        form = PacientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_pacienti')
+    else:
+        form = PacientForm()
+    
+    return render(request, 'pacienti/adauga_pacient.html', {'form': form})
